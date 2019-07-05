4912c90824eca4faa03e48f9654b27053f8d2473
Z.H Yang <younggakki@protonmail.com>
2019-07-05 14:34:32 +0800

    003year judge year is leap year or not, add number check

--- 003year.py
+++ 003year.py
@@ -4,8 +4,12 @@ judge the year is leap year or not
 """
 
 while 1:
-    year = int(input('type year to check leap year or not: '))
-    if year % 400 or year % 4 == 0 and year % 100 != 0:
-        print('yes')
+    year = (input('type year to check leap year or not: '))
+    if str.isdigit(year):
+        year = int(year)
+        if year % 400 or year % 4 == 0 and year % 100 != 0:
+            print('yes')
+        else:
+            print('not')
     else:
-        print('not')
+        print('please type in a number')
