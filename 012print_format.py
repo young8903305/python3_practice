d9fc72045c3f27b8dbaac038939e345873d3c709
Z.H Yang <younggakki@protonmail.com>
2020-02-06 15:12:14 +0800

    edit 012 commits

--- 012print_format.py
+++ 012print_format.py
@@ -1,22 +1,22 @@
 """
-use four different way to concate string, then print them out.
+use four different ways about string concatenation, then print them out.
 """
 
 first_name = 'Zeth'
 last_name = 'Yang'
 
-# string concatination with +
+# string concatenation with +
 output1 = 'Hello, ' + first_name + ' ' + last_name
 print(output1)
 
-# string concatination with format function
+# string concatenation with format function
 output2 = 'Hello, {} {}'.format(first_name, last_name)
 print(output2)
 
-# string concatination with format function and index, you can change the order of your parameters
+# string concatenation with format function and index, you can change the order of your parameters
 output3 = 'Hello, {1} {0}'.format(first_name, last_name)
 print(output3)
 
-# string concatination with "cool" format function 
+# string concatenation with "cool" format function 
 output4 = f'Hello, {first_name} {last_name}'
 print(output4)
