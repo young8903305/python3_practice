8f6c6c51f6f98b7f194c4cfa158f2854fed8d140
Z.H Yang <younggakki@protonmail.com>
2020-02-10 15:51:39 +0800

    add try-except-rase-finally exception handling example

---
+++ 016ExceptionHandling.py
@@ -0,0 +1,47 @@
+def try_finally():
+    try:
+        print('This is a try-finally statement')
+    finally:
+        print('finally is executed no matter what, and is generally used to release external resources.')
+
+
+def try_except():
+    num_list = [2, 3, 0]
+    try:
+        print('This is a try-exception statement')
+        for num in num_list:
+            print(f'6/{num} = {int(6/num)}')
+    except ZeroDivisionError:
+        print('Oops! <Class \'ZeroDivisionError\'> occured. you divide 0')
+
+def try_except_finally():
+    num_list = [2, 3, 0]
+    try:
+        print('This is a try-exception statement')
+        for num in num_list:
+            print(f'6/{num} = {int(6/num)}')
+    except ZeroDivisionError:
+        print('Oops! <Class \'ZeroDivisionError\'> occured. you divide 0')
+    finally:
+        print('This is finally statement.')
+
+# Exceptions are raised when corresponding errors occur at run time, but we can forcefully raise it using the keyword raise.
+# We can also optionally pass in value to the exception to clarify why that exception was raised.
+def try_raise_except():
+    try:
+        positive_num = int(input("Enter a positive integer: "))
+        if positive_num <= 0:
+            raise ValueError("That is not a positive number!")
+    except ValueError as ve:
+        print(ve)
+
+if __name__ == '__main__':
+    try_finally()
+    print()
+    try_except()
+    print()
+    try_except_finally()
+    print()
+    try_raise_except()
+
+
