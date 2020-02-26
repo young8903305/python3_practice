0953341a1dc82d3cf2e990940b3b6e0f9990bccd
Z.H Yang <younggakki@protonmail.com>
2020-02-26 21:36:22 +0800

    add comment, change file path to a parameter

--- 027custom_encode_decode_json.py
+++ 027custom_encode_decode_json.py
@@ -6,10 +6,10 @@ Complex Object encoding use JSONEncoder
 
 import json
 
-
+# custom JSON encoding by using JSONEncoder
 class ComplexEncoder(json.JSONEncoder):
     def default(self, z):
-        if isinstance(z, complex):
+        if isinstance(z, complex):    #if we encounter a complex object
             return (z.real, z.imag)
         else:
             return super().default(z)
@@ -22,7 +22,8 @@ def decode_complex(dct):
 json_string = json.dumps(2 + 5j, cls=ComplexEncoder)
 print(json_string)
 
-with open('027example_data.json') as complex_data:
+file_path = './027example_data_00.json'
+with open(file_path) as complex_data:
     data = complex_data.read()
     z = json.loads(data, object_hook=decode_complex)
 print(z)
