b9436858c941bbf393d013b8fe8548fefae38b2e
Z.H Yang <younggakki@protonmail.com>
2020-03-06 11:10:22 +0800

    Palindrome Number

---
+++ 031Palindrome_Number.py
@@ -0,0 +1,20 @@
+class Solution:
+    def isPalindrome(self, x: int) -> bool:
+        if x < 10 and x >= 0:
+            return True
+        if x < 0:
+            return False
+        
+        result = 0
+        remain = 0
+        in_x = x
+        while x != 0:
+            remain = x % 10
+            result = result * 10 + remain
+            x = x // 10
+        
+        
+        if result != in_x:
+            return False
+        else:
+            return True
